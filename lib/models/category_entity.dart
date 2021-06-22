b3a458058b2c3db021e121ab5b611a8eb127f9b1
Shvet <dhavalgondaliya55@gmail.com>
2021-06-22 11:54:44 +0530

    Changes at 22-06-2021

---
+++ lib/models/category_entity.dart
@@ -0,0 +1,40 @@
+import 'package:enciclopiedia_deportiva/generated/json/base/json_convert_content.dart';
+import 'package:enciclopiedia_deportiva/generated/json/base/json_field.dart';
+
+class CategoryEntity with JsonConvert<CategoryEntity> {
+  String? id;
+  @JSONField(name: "asset_id")
+  String? assetId;
+  @JSONField(name: "parent_id")
+  String? parentId;
+  String? lft;
+  String? rgt;
+  String? level;
+  String? path;
+  String? extension;
+  String? title;
+  String? alias;
+  String? note;
+  String? description;
+  String? published;
+  @JSONField(name: "checked_out")
+  String? checkedOut;
+  @JSONField(name: "checked_out_time")
+  String? checkedOutTime;
+  String? access;
+  String? params;
+  String? metadesc;
+  String? metakey;
+  String? metadata;
+  @JSONField(name: "created_user_id")
+  String? createdUserId;
+  @JSONField(name: "created_time")
+  String? createdTime;
+  @JSONField(name: "modified_user_id")
+  String? modifiedUserId;
+  @JSONField(name: "modified_time")
+  String? modifiedTime;
+  String? hits;
+  String? language;
+  String? version;
+}
